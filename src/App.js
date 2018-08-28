b2e998f8df675fe9deaa89806f8a2396545fefef
Damien Ching <damienching@gmail.com>
2018-08-28 17:58:07 +1000

    card grid and cards closes #11

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import JssProvider from 'react-jss/lib/JssProvider';
+import { create } from 'jss';
+import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
 import './App.css';
 import ApiTokenDialog from './components/ApiTokenDialog';
+import CardGrid from './containers/cardGridContainer'
 import PaginationControls from './containers/paginationControlsContainer';
+
+const generateClassName = createGenerateClassName();
+const jss = create(jssPreset());
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PaginationControls/>
-        <ApiTokenDialog/>
-      </div>
+      <JssProvider jss={jss} generateClassName={generateClassName}>
+        <div className="App">
+          <CardGrid />
+          <PaginationControls />
+          <ApiTokenDialog />
+        </div>
+      </JssProvider>
     );
   }
 }
---
+++ src/containers/cardGridContainer.js
@@ -0,0 +1,4 @@
+import { connect } from 'react-redux'
+import cardGrid from '../components/cardGrid'
+
+export default connect((state) => {return {cards: state.cards}})(cardGrid);
