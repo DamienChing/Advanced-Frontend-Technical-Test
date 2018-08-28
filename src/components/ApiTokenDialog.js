3de56223f6d8d9d803e42a4e29e0836093f88eb3
Damien Ching <damienching@gmail.com>
2018-08-28 12:48:58 +1000

    API Token Dialog resolves #1

---
+++ src/components/ApiTokenDialog.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+
+export default class ApiTokenDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.apiToken = React.createRef();
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault(); //prevent form from linking to page
+    sessionStorage.setItem('apiToken', this.apiToken.value);
+    this.forceUpdate(); //close dialog by refreshing open prop
+  }
+
+  render() {
+    return (
+      <Dialog open={!sessionStorage.getItem('apiToken')}>
+        <DialogTitle id="apitoken-dialog-title">Insert API Token</DialogTitle>
+        <DialogContent>
+            <DialogContentText>
+              To subscribe to this website, please enter your email address here. We will send
+              updates occasionally.
+            </DialogContentText>
+            <form onSubmit={this.onSubmit}>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="API Token"
+                type="text"
+                fullWidth
+                inputRef={e => this.apiToken = e}
+              />
+            </form>
+          </DialogContent>
+        <DialogActions>
+          <Button onClick={this.onSubmit} color="primary">
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    )
+  }
+}
