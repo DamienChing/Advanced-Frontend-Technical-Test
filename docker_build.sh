738e97f838b0bfa31296157a62b4b5b8a0a6d4e8
Damien Ching <damienching@gmail.com>
2018-09-02 23:55:54 +1000

    re build commands to docker build

--- docker_build.sh
+++ docker_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ -d build ]; then
 	echo -e "Build folder detected.\nUsing existing build files for docker image: $1\n"
-	# npm install
-	# npm run build
+	npm install
+	npm run build
 	docker build -f nginx.Dockerfile -t $1 .
 else
 	echo -e "Build folder not detected.\nUsing two stage build for docker image: $1\n"
